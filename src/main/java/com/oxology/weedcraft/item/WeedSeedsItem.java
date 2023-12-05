b5741c9dd0ffe7c4e1d4becf385848397946e32e
Masiu121 <maks.kapolka@gmail.com>
2023-12-05 09:36:26 +0100

    Fixed seeds to work with crop

--- src/main/java/com/oxology/weedcraft/item/WeedSeedsItem.java
+++ src/main/java/com/oxology/weedcraft/item/WeedSeedsItem.java
@@ -1,9 +1,11 @@
 package com.oxology.weedcraft.item;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 
-public class WeedSeedsItem extends Item {
-    public WeedSeedsItem() {
-        super(new Properties());
+public class WeedSeedsItem extends BlockItem {
+    public WeedSeedsItem(Block block) {
+        super(block, new Properties());
     }
 }
