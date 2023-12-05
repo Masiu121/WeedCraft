37020ac80aa34300f14350a3d6091a407d738bde
Masiu121 <maks.kapolka@gmail.com>
2023-12-05 09:37:46 +0100

    Changed crop block name

--- src/main/java/com/oxology/weedcraft/block/WeedCraftBlocks.java
+++ src/main/java/com/oxology/weedcraft/block/WeedCraftBlocks.java
@@ -11,5 +11,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class WeedCraftBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, WeedCraft.MOD_ID);
 
-    public static final RegistryObject<Block> WEED_FLOWER = BLOCKS.register("weed_crop", () -> new WeedCropBlock(AbstractBlock.Properties.copy(Blocks.WHEAT).noOcclusion().noCollission()));
+    public static final RegistryObject<Block> WEED_CROP = BLOCKS.register("weed_crop", () -> new WeedCropBlock(AbstractBlock.Properties.copy(Blocks.WHEAT).noOcclusion().noCollission()));
 }
